01e531b5fd5fa1ac33b2816fdc70d5cdbc47fe64
Marco <marcogreselin@me.com>
2016-07-03 14:52:13 +0100

    acceptable console

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,28 @@ router.get('/signup/publisher', function(req, res, next) {
   res.render('signup-publisher');
 });
 
+// console routes
 router.get('/console', function(req, res) {
-  res.render('console');
+  res.render('console/');
 });
+
+// console advertiser
+router.get('/console/create-ad', function(req, res) {
+  res.render('console/create-ad');
+});
+
+router.get('/console/answer', function(req, res) {
+  res.render('console/create-ad');
+});
+
+router.get('/console/my-ads', function(req, res) {
+  res.render('console/create-ad');
+});
+
+
+// console publisher
+router.get('/console/my-snippet', function(req, res) {
+  res.render('console/create-ad');
+});
+
 module.exports = router;
