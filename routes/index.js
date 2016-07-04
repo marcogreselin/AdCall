0500beec3ba0acea89d0bedbf4018193138202c5
Marco <marcogreselin@me.com>
2016-07-04 19:17:22 +0100

    minimal

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
 // GET login page.
 router.get('/login', function(req, res, next) {
   res.render('login');
