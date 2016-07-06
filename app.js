1e56c543e467d6927ab2b3914b961bfe5c19dcff
Marco <marcogreselin@me.com>
2016-07-06 22:55:04 +0100

    minor

--- app.js
+++ app.js
@@ -20,12 +20,11 @@ app.set('view engine', 'ejs');
 app.use(favicon(path.join(__dirname, 'public','images', 'favicon.ico')));
 
 // // Redirect http requests from http://jaketrent.com/post/https-redirect-node-heroku/
-// app.use(function(req, res, next) {
-//   if(req.header['x-forwarded-proto'] != 'https') {
-//     return res.redirect('https://' + req.get('Host') + req.url);
-//   }
-//   next();
-// });
+app.use(function(req, res, next) {
+  if(req.header['x-forwarded-proto'] != 'https') {
+    res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+});
 
 // app password protection
 var auth = function (req, res, next) {
