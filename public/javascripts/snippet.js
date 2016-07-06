4b965528cac5da50ad133f43793ab6349e50e146
Marco <marcogreselin@me.com>
2016-07-06 18:07:37 +0100

    minor

---
+++ public/javascripts/snippet.js
@@ -0,0 +1,20 @@
+/**
+ * Created by marco on 06/07/2016.
+ */
+
+
+var video = document.querySelector("#videoElement");
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+
+if (navigator.getUserMedia) {
+    navigator.getUserMedia({video: true}, handleVideo, videoError);
+}
+
+function handleVideo(stream) {
+    video.src = window.URL.createObjectURL(stream);
+}
+
+function videoError(e) {
+    // do something
+}
