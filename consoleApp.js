73d294c340d5b0486286891cff4a4b7f1e9c8c9c
Marco <marcogreselin@me.com>
2016-07-08 21:16:27 +0100

    minor

--- consoleApp.js
+++ consoleApp.js
@@ -7,7 +7,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/src/views'));
 app.set('view engine', 'ejs');
 
 var router = express.Router();
