0814f8ebfb07ee76481fbd5a14c0842c1b94ee46
Marco <marcogreselin@me.com>
2016-07-10 17:26:28 +0100

    form validation

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,38 +3,36 @@ var router = express.Router();
 var pg = require('pg');
 
 router.route('/signup')
-    .post(function (req, res) {
-        console.log(req.body);
-
-
-
+    .post(function (req, res, next) {
         pg.defaults.ssl = true;
-        pg.connect(process.env.DATABASE_URL || 'postgres://ugeiskcgfndzuy:2mReS0WnS_ob7pWjEkndIyrPDl@ec2-54-247-185-241.eu-west-1.compute.amazonaws.com:5432/ddm2it63dsusah', function(err, client) {
-            if (err) throw err;
-            console.log('Connected to postgres! Getting schemas...');
+        pg.connect(process.env.DATABASE_URL || `postgres://ugeiskcgfndzuy:2mReS0WnS_ob7pWjEkndIyrPDl@ec2-54-247-185-241.`+
+            `eu-west-1.compute.amazonaws.com:5432/ddm2it63dsusah`, function(err, client) {
+            if (err){
+                console.log(err['detail']);
+                res.status(500).send('The connection to the database went wrong.');
+                console.log('first');
+            } else {
+                client
+                    .query(`INSERT INTO agent (admin, email, password, firstname, lastname)
+                VALUES (false, '${req.body.email}', '${req.body.password}', '${req.body.firstname}', '${req.body.lastname}');`, function(err, result) {
+                        if(err.constraint){
+                            console.log(err);
+                            res.status(500).send("This email address is already registered with us.");
+                        } else if (err){
+                            console.log(err);
+                            res.status(500).send("Something is wrong. Contact us!");
+                        }
+                    });
+            }
 
-            client
-                .query(`INSERT INTO agent (admin, email, password, firstname, lastname, position) 
-                VALUES (true, 'marcogresemnmlin@me.com\', 'gallome33-', 'Marco', 'Greselin', 'CEO');`, function(err, result) {
-                    if(err){
-                        console.log(err);
-                    }
-                })
-                .on('row', function(row) {
-                    console.log(JSON.stringify(row));
-                });
         });
-
-
-        req.login(req.body, function(){
-            res.redirect('/auth/profile');
-        });
+        // req.login(req.body, function(){
+        //     res.redirect('/auth/profile');
+        // });
     });
 router.route('/profile')
     .get(function(req, res){
         res.json(req.user);
-
-
     });
 
 module.exports = router;
